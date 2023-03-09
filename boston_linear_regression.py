708dae6f766e556ec4918022e7e8709b430a4034
ahsan-ali49 <ahsanali.bscssef20@iba-suk.edu.pk>
2023-03-09 17:02:03 +0500

    refactored

--- boston_linear_regression.py
+++ boston_linear_regression.py
@@ -1,20 +1,14 @@
-#%% Importing all the libraries for; splitting data, running linear regression and plotting graphs
-import random
+#%% Importing all the libraries for; shuffling & splitting data, running linear regression and plotting graphs
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 #%% Loading Datasets
 boston_data = pd.read_csv(r"C:\Users\Ahsan\Desktop\AIcodes\housing.csv")
-#%% Seperating independent and dependent variables respectively (X=features(13), y=target(median value of home in $1000))
-X = boston_data.iloc[:, :-1]
-y = boston_data.iloc[:,-1]
-print(boston_data.shape)
 
-#%% Converting to list and print its dimensions
-tuples = list(zip(X, y))
+#%% Printing dimensions of dataset
 print(boston_data.shape)
 
 #%% Creating seed values, and lists for MSE and Regression score
@@ -24,21 +18,25 @@ r2_scores = []
 
 #%% Shuffling & Spitting data 10 times, running linearRegression and predicting with R2 and MSE
 for seed in seeds:
+    # Shuffling & Seperating independent & dependent variables respectively (X=features(13), y=target(median value of home in $1000))    
+    shuffled_data = boston_data.sample(frac=1, random_state=(seed))
+    X = shuffled_data.iloc[:, :-1]
+    y = shuffled_data.iloc[:,-1]
     
-    # Setting seed values and shuffling
-    random.seed(seed)
-    random.shuffle(tuples)
+    #print(tuples)
+    print(X)
     
     # Splitting data in ratio of 0.7 for training and 0.3 for testing
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=(seed))
+    X_train, X_test, y_train, y_test = train_test_split(X,y,  test_size=0.3, random_state=(seed))
     print("Seed", seed, ": Train set size =", len(X_train), ", Test set size =", len(X_test))
     
     # Running LinearRegression
     model = LinearRegression()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
+    
     # getting R2 score for each sample of data
-    score = model.score(X_test, y_test)
+    score = r2_score(y_test, y_pred)
     r2_scores.append(score)
     print("Seed", seed, "Regression score:", score)
     
